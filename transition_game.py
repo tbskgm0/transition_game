e2604a809ae3e129a1cf71c1b62636b354aa2ec4
tbskgm0 <tsubasa.kogoma@axia-works.com>
2022-11-04 11:31:21 +0900

    refactor: 関数の追加

--- transition_game.py
+++ transition_game.py
@@ -17,7 +17,7 @@ def calculation_distance(position1, position2):
 def move_position(direction, position):
     current_x, current_y = position
 
-    match user_input:
+    match direction:
         case "n":
             current_y = current_y - 1
         case "s":
@@ -31,23 +31,25 @@ def move_position(direction, position):
 
 BAORD_SIZE = 5
 
-watermelon_position = generate_position(BAORD_SIZE)
-print("スイカ初期位置：", watermelon_position)
+def suika_wari():
+    watermelon_position = generate_position(BAORD_SIZE)
+    print("スイカ初期位置：", watermelon_position)
 
-player_position = generate_position(BAORD_SIZE)
-print("プレイヤー初期位置：", player_position)
+    player_position = generate_position(BAORD_SIZE)
+    print("プレイヤー初期位置：", player_position)
+
+    while watermelon_position == player_position:
+        player_position = generate_position(BAORD_SIZE)
+        print("スイカと同じ位置だったので、プレイヤーの場所を変更しました：", watermelon_position)
+
+    while watermelon_position != player_position:
+        distance = calculation_distance(player_position, watermelon_position)
+        print("スイカまでの距離:", distance)
+
+        user_input = input("n:北に移動 s:南に移動 e:東に移動 w:西に移動")
+        player_position = move_position(user_input, player_position)
+
+    print("スイカが割れました！！")
 
 # 実装部分
-
-while watermelon_position == player_position:
-    player_position = generate_position(BAORD_SIZE)
-    print("スイカと同じ位置だったので、プレイヤーの場所を変更しました：", watermelon_position)
-
-while watermelon_position != player_position:
-    distance = calculation_distance(player_position, watermelon_position)
-    print("スイカまでの距離:", distance)
-
-    user_input = input("n:北に移動 s:南に移動 e:東に移動 w:西に移動")
-    player_position = move_position(user_input, player_position)
-
-print("スイカが割れました！！")
+suika_wari()
